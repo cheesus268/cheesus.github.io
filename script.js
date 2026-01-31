4ed3760e78cf7af17a000e0e4d84828c5c1a472d
cheesus <hochsehslion09@gmail.com>
2026-02-01 00:35:10 +0100

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const originalHeight = window.visualViewport.height;
 let input = document.getElementById("input");
 let button = document.getElementById("add");
 let list = document.getElementById("items");
@@ -31,21 +30,6 @@ button.addEventListener("click", () => {
 input.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         button.click();
-    }
-});
-
-input.addEventListener("focus", () => {
-    input.scrollIntoView({
-        behavior: "smooth",
-        block: "center"
-    });
-});
-
-window.visualViewport.addEventListener("resize", () => {
-    if (Math.abs(window.visualViewport.height - originalHeight) < 20) {
-        if (document.activeElement.tagName === "INPUT") {
-            document.activeElement.blur();
-        }
     }
 });
 
